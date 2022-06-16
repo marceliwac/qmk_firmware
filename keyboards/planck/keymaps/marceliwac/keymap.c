064228454178f34331ed92ace9d82ae8d020b13a
Marceli Wac <marceliwac@gmail.com>
2022-06-16 18:24:31 +0100

    feat: Remove unncecessary layer toggles.

--- keyboards/planck/keymaps/marceliwac/keymap.c
+++ keyboards/planck/keymaps/marceliwac/keymap.c
@@ -163,10 +163,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    RESET,   RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    RESET,   _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  RGB_SAI,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, RGB_SAD,
-    _______, _______, _______, _______, _______, RGB_TOG, RGB_TOG, _______, RGB_HUD,  RGB_VAD, RGB_VAI ,RGB_HUI
+    RESET,   RESET,   DEBUG,   _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SAI,
+    AU_TOG,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SAD,
+    MU_TOG,  _______, _______, _______, _______, RGB_TOG, RGB_TOG, _______, RGB_HUD, RGB_VAD, RGB_VAI, RGB_HUI
+                               
+//  RESET,   RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
+//  RESET,   _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  RGB_SAI,
+//  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, RGB_SAD,
+//  _______, _______, _______, _______, _______, RGB_TOG, RGB_TOG, _______, RGB_HUD,  RGB_VAD, RGB_VAI, RGB_HUI
 )
 
 };
